1cccaa44dc9257621ae797eeb17c533fb55413be
chase930 <mchase7512@gmail.com>
2024-08-16 19:30:39 +0900

    Initialization

--- server/fetch_sec_data.py
+++ server/fetch_sec_data.py
@@ -1,7 +1,11 @@
-from sec_api import QueryApi #type: ignore
-import pandas as pd #type: ignore
+import os
+from sec_api import QueryApi
+import pandas as pd
+from dotenv import load_dotenv
 
-queryApi = QueryApi(api_key="your_api_key")
+load_dotenv()
+
+queryApi = QueryApi(api_key=os.getenv("SEC_API_KEY"))
 
 query = {
     "query": { "query_string": { "query": "formType:\"4\"" } },
@@ -12,6 +16,16 @@ query = {
 
 filings = queryApi.get_filings(query)
 
-df = pd.json_normalize(filings['filings'])
+print(filings)
 
-print(df[['reportingOwnerName', 'transactionDate', 'transactionShares']])
+if filings and 'filings' in filings:
+    df = pd.json_normalize(filings['filings'])
+    print(df.columns)  
+    print(df.head())   
+    
+    if all(col in df.columns for col in ['reportingOwnerName', 'transactionDate', 'transactionShares']):
+        print(df[['reportingOwnerName', 'transactionDate', 'transactionShares']])
+    else:
+        print("Columns not found in DataFrame.")
+else:
+    print("No filings returned from the API.")
