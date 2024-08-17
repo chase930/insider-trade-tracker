8056fbb3676c6d94b9f3a72be7945c586ed753da
chase930 <mchase7512@gmail.com>
2024-08-17 13:20:10 +0900

    I was supposed to use InsiderTradingApi not QueryApi

--- server/fetch_sec_data.py
+++ server/fetch_sec_data.py
@@ -1,54 +1,22 @@
 import os
-from sec_api import QueryApi  # type: ignore
+from sec_api import InsiderTradingApi #type: ignore
 import pandas as pd  # type: ignore
 from dotenv import load_dotenv
 import json
 
 load_dotenv()
 
-queryApi = QueryApi(api_key=os.getenv("SEC_API_KEY"))
+insiderTradingApi = InsiderTradingApi(api_key=os.getenv("SEC_API_KEY"))
 
-query = {
-    "query": {"query_string": {"query": "formType:\"4\""}},
-    "from": "0",
-    "size": "20",
-    "sort": [{"filedAt": {"order": "desc"}}]
-}
 
-filings = queryApi.get_filings(query)
+query_string = "issuer.tradingSymbol:TSLA"
 
-print(json.dumps(filings['filings'][0], indent=4))
+insider_trades_sample = insiderTradingApi.get_data({
+  "query": {"query_string": {"query": query_string}},
+  "from": "0",
+  "size": "2",
+  "sort": [{ "filedAt": { "order": "desc" } }]
+})
 
-def get_reporting_owner_data(filing):
-    company_name = None
-    insider_name = None
+print(json.dumps(insider_trades_sample, indent=4))
 
-    for entity in filing['entities']:
-        if '(Issuer)' in entity['companyName']:
-            company_name = entity['companyName'].replace(' (Issuer)', '').strip()
-        if '(Reporting)' in entity['companyName']:
-            insider_name = entity['companyName'].replace(' (Reporting)', '').strip()
-
-    return {
-        'companyName': company_name,
-        'insider_reportingOwnerName': insider_name,
-        'filedAt': filing['filedAt']
-    }
-
-data = [get_reporting_owner_data(filing) for filing in filings['filings']]
-
-df = pd.DataFrame(data)
-
-print("\nRelevant filing data:")
-print(df)
-
-entities_df = pd.json_normalize(
-    filings['filings'],
-    'entities',
-    ['companyName', 'formType', 'filedAt'],
-    record_prefix='entity_',
-    meta_prefix='parent_'
-)
-
-print("\nEntities details:")
-print(entities_df.head())
